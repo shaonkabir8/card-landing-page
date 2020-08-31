b255fc725291c3c3bc7bb5b23cf4d7b7b26e289e
shaonkabir8 <shaonkabir98@gmail.com>
2020-09-01 05:02:30 +0600

    refactor: add Footer Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Card from "./Card";
 import Layout from "./Layout";
+import Card from "./Card";
+import Footer from "./Footer";
 
 const app = () => (
   <Layout>
     <Card />
+    <Footer />
   </Layout>
 );
 
