5494f30343d971a605d4025c32145ee1749b5e97
shaonkabir8 <shaonkabir98@gmail.com>
2025-12-15 00:09:39 +0600

    feat: setup landing page boilerplate with modern design

--- src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const layout = ({ children }) => (
-  <main className="container">
-    <div className="container__inner">{children}</div>
-  </main>
-);
-
-export default layout;
